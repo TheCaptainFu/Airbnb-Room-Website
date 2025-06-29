e44041223e9660c86f9934e5ee11338d82ec79b1
TheCaptainFu <g.gsmsk@gmail.com>
2025-06-29 14:26:26 +0300

    Almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./html/**/*.{html,js}", "./js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-grey': '#6b7280', // gray-500 equivalent
+        'custom-gold': '#fbbf24', // yellow-400 equivalent
+        'custom-dark-grey': '#242424', // dark grey
+      },
+    },
   },
   plugins: [],
 }
